dc3ac7ef8fae2556d5d67001b562ed8697526bca
Anton Korobenko <anton.koorobenko@gmail.com>
2021-10-04 17:06:28 +0300

    Working local app. Added html and css.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+const express = require("express");
+const app = express();
+
+app.use("/static", express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get('/',(req, res) => {
+    res.render('todo.ejs')
+    })
+
+app.listen(8000, () => console.log("Server Up and running"));
